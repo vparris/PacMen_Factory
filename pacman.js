4b1a2eabbe46535f86f1560decd358b062dd15b7
Victoria Parris <84088822+vparris@users.noreply.github.com>
2021-08-03 14:12:56 -0400

    Add files via upload

---
+++ pacman.js
@@ -0,0 +1,82 @@
+var pos = 0;
+let pageWidth = window.innerWidth;
+const pacArray = [
+  ['PacMan1.png', 'PacMan2.png'],
+  ['PacMan3.png', 'PacMan4.png'],
+];
+
+var direction = 0;
+var focus = 0;
+const pacMen = [];
+
+function setToRandom(scale) {
+  return {
+    x: Math.random() * scale,
+    y: Math.random() * scale,
+  };
+}
+
+//Factory to make pacman
+function makePac() {
+  // returns an object with values scaled {x: 33, y: 21}
+  let velocity = setToRandom(10);
+  let position = setToRandom(200);
+
+  // Add image to div id = game
+  focus = (focus +1) %2;
+  let game = document.getElementById("game");
+  let newimg = document.createElement("img");
+  newimg.style.position = "absolute";
+  newimg.src = pacArray[direction][focus];
+  newimg.width = 100;
+  newimg.style.left = position.x;
+  newimg.style.top = position.y;
+  newimg.style.zIndex = -1;
+  game.appendChild(newimg);
+
+  // new style of creating an object
+  return {
+    position,
+    velocity,
+    newimg,
+  };
+  
+}
+
+
+function update() {
+  //loop over pacmen array and move each one and move image in DOM
+  pacMen.forEach((item) => {
+    checkCollisions(item);
+    item.position.x += item.velocity.x;
+    item.position.y += item.velocity.y;
+
+    item.newimg.style.left = item.position.x;
+    item.newimg.style.top = item.position.y;
+  });
+  setTimeout(update, 20);
+}
+
+function reset(){
+    location.reload();
+}
+
+  
+
+function checkCollisions(item) {
+  if (
+    item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
+    item.position.x + item.velocity.x < 0
+  )
+    item.velocity.x = -item.velocity.x;
+  if (
+    item.position.y + item.velocity.y + item.newimg.height >
+      window.innerHeight ||
+    item.position.y + item.velocity.y < 0
+  )
+    item.velocity.y = -item.velocity.y;
+}
+
+function makeOne() {
+  pacMen.push(makePac()); // add a new PacMan
+}
